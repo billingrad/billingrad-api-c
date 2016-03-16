135f11c20f2cd8484e10e895a8539ca101e2780d
Roman Sivakov <romansivakov@gmail.com>
2016-03-16 16:34:46 +0300

    First

---
+++ bg-api/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Net;
+using System.IO;
+
+namespace bg_api
+{
+    class Program
+    {
+        private static string host = "http://my.billingrad.com/api/";
+        private static string open = "";
+        private static string close = "";
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 5)
+            {
+                System.Console.WriteLine("Usage: bg_api.exe open close api fn \"json_data\"");
+                return;
+            }
+
+            open = args[0];
+            close = args[1];
+
+            string api = args[2];
+            string fn = args[3];
+            string json_data = args[4];
+
+            //string key = Base64Encode(sha256($this->close. $data, true));
+            string key = sha256(close + json_data);
+
+
+            string url = host + api + '/' + fn + "?_open=" + EscapeUriDataStringRfc3986(open) + "&_key=" + EscapeUriDataStringRfc3986(key);
+
+
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            var data = Encoding.ASCII.GetBytes(json_data);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            System.Console.WriteLine(responseString);
+        }
+
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        private static string sha256(string password)
+        {
+            SHA256Managed crypt = new SHA256Managed();
+            string hash = String.Empty;
+            byte[] crypto = crypt.ComputeHash(Encoding.ASCII.GetBytes(password), 0, Encoding.ASCII.GetByteCount(password));
+            foreach (byte theByte in crypto)
+            {
+                hash += theByte.ToString("x2");
+            }
+            return hash;
+        }
+
+        /// <summary>
+        /// The set of characters that are unreserved in RFC 2396 but are NOT unreserved in RFC 3986.
+        /// </summary>
+        private static readonly string[] UriRfc3986CharsToEscape = new[] { "!", "*", "'", "(", ")" };
+
+        /// <summary>
+        /// Escapes a string according to the URI data string rules given in RFC 3986.
+        /// </summary>
+        /// <param name="value">The value to escape.</param>
+        /// <returns>The escaped value.</returns>
+        /// <remarks>
+        /// The <see cref="Uri.EscapeDataString"/> method is <i>supposed</i> to take on
+        /// RFC 3986 behavior if certain elements are present in a .config file.  Even if this
+        /// actually worked (which in my experiments it <i>doesn't</i>), we can't rely on every
+        /// host actually having this configuration element present.
+        /// </remarks>
+        internal static string EscapeUriDataStringRfc3986(string value)
+        {
+            // Start with RFC 2396 escaping by calling the .NET method to do the work.
+            // This MAY sometimes exhibit RFC 3986 behavior (according to the documentation).
+            // If it does, the escaping we do that follows it will be a no-op since the
+            // characters we search for to replace can't possibly exist in the string.
+            StringBuilder escaped = new StringBuilder(Uri.EscapeDataString(value));
+
+            // Upgrade the escaping to RFC 3986, if necessary.
+            for (int i = 0; i < UriRfc3986CharsToEscape.Length; i++)
+            {
+                escaped.Replace(UriRfc3986CharsToEscape[i], Uri.HexEscape(UriRfc3986CharsToEscape[i][0]));
+            }
+
+            // Return the fully-RFC3986-escaped string.
+            return escaped.ToString();
+        }
+    }
+}
